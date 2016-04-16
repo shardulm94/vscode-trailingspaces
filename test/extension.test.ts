e03cea021e202ab5987a7228f9f24d392912e4b2
shardulm94 <shardul.m@somaiya.edu>
2016-04-16 23:12:45 +0530

    Convert from thenable to timeout #2

--- test/extension.test.ts
+++ test/extension.test.ts
@@ -99,11 +99,12 @@ describe("Extension Tests", () => {
 
 let assertDeleteTrailingSpaces = (editor: vscode.TextEditor, settings: TralingSpacesSettings, expectedOutputFile: string, done: MochaDone): void => {
     let outputFile: string = fs.readFileSync(path.join(__dirname, expectedOutputFile), "utf-8");
-    vscode.commands.executeCommand("trailing-spaces.setSettings", settings).then(() => {
+    vscode.commands.executeCommand("trailing-spaces.setSettings", settings);
+    setTimeout(() => {
         vscode.commands.executeCommand("trailing-spaces.deleteTrailingSpaces");
         setTimeout(() => {
             assert.equal(editor.document.getText(), outputFile);
             done();
         }, 200);
-    });
+    }, 200);
 };
